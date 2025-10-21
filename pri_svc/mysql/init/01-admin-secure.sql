a064195389a2e789707f67fc1bff5eea8a6506ee
lucas <fallsky104@gmail.com>
2025-10-21 23:38:12 +0900

    Update 01-admin-secure.sql

--- pri_svc/mysql/init/01-admin-secure.sql
+++ pri_svc/mysql/init/01-admin-secure.sql
@@ -1,5 +1,5 @@
 -- 1) root는 "컨테이너 내부(localhost) 전용"으로만 유지
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'root1234!';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '';
 
 -- 혹시라도 만들어졌을 수 있는 원격 root들을 제거(존재해도 안전; 없으면 경고만)
 DROP USER IF EXISTS 'root'@'%';
@@ -16,11 +16,11 @@ FLUSH PRIVILEGES;
 
 -- 2) 도커 네트워크 대역에서만 붙는 전용 관리 계정 생성
 --    예: DBGate가 붙을 계정. 호스트에 와일드카드 사용 가능
-CREATE USER IF NOT EXISTS 'gate_admin'@'192.168.%' IDENTIFIED BY 'gate_pass';
-GRANT ALL PRIVILEGES ON *.* TO 'gate_admin'@'192.168.%';
--- 필요 시 특정 DB로만 축소: GRANT ALL ON appdb.* TO 'dbgate_admin'@'172.22.%';
+CREATE USER IF NOT EXISTS ''@'192.168.%' IDENTIFIED BY '';
+GRANT ALL PRIVILEGES ON *.* TO ''@'192.168.%';
+-- 필요 시 특정 DB로만 축소: GRANT ALL ON appdb.* TO ''@'172.22.%';
 
 -- (선택) 네트워크 내부라도 전송 암호화를 강제하고 싶다면 아래 사용(서버 TLS 설정 필요)
--- ALTER USER 'dbgate_admin'@'172.22.%' REQUIRE SSL;
+-- ALTER USER ''@'172.22.%' REQUIRE SSL;
 
 FLUSH PRIVILEGES;
